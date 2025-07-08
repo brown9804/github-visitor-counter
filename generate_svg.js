48b7feb8f962834f2c8f01a60c895742d35c2e84
Timna Brown <24630902+brown9804@users.noreply.github.com>
2025-07-07 21:23:03 -0600

    name changed

--- generate_svg.js
+++ generate_svg.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 const REPO = process.env.REPO || 'brown9804/github-visitor-counter';
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GITHUB_TOKEN = process.env.TRAFFIC_TOKEN
+
 if (!GITHUB_TOKEN) {
   console.error('Error: GITHUB_TOKEN environment variable is not set.');
   process.exit(1);
